9b1120c3fb795419941401a4f23de953f9f08a8a
yamachu <yamachu.dev@gmail.com>
2019-01-25 02:40:29 +0900

    Azure Functionsのインタフェースをごにょごにょ

---
+++ azure/src/main/scala/com/example/azure/Handler.scala
@@ -0,0 +1,41 @@
+package com.example.azure
+
+import java.util._
+
+import com.example._
+import com.example.azure.utils.Logger
+import com.example.objects.Requests
+import com.microsoft.azure.functions._
+import com.microsoft.azure.functions.annotation._
+
+import collection.JavaConverters._
+
+class Handler {
+  @FunctionName("HttpHandlerGET")
+  def runHttpGet(@HttpTrigger(
+                   name = "req",
+                   methods = Array(HttpMethod.GET),
+                   authLevel = AuthorizationLevel.ANONYMOUS
+                 ) request: HttpRequestMessage[Optional[String]],
+                 context: ExecutionContext): HttpResponseMessage = {
+    val logger = Logger(context.getLogger)
+    logger.info("Scala HTTP GET trigger processed a request.")
+
+    // Parse query parameter
+    val queryMap: collection.mutable.Map[String, String] =
+      request.getQueryParameters.asScala
+
+    (for {
+      requests <- Requests.fromMap(queryMap)
+      result   <- Functions(logger).run(requests)
+    } yield result) match {
+      case Right(s) =>
+        request.createResponseBuilder(HttpStatus.OK).body(s).build
+      case Left(e) =>
+        request
+          .createResponseBuilder(HttpStatus.BAD_REQUEST)
+          .body(e.toString)
+          .build
+    }
+  }
+}
