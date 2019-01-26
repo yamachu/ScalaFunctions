19796812eb111fbdd995312cbf2e215453d75c02
yamachu <yamachu.dev@gmail.com>
2019-01-27 02:09:44 +0900

    POSTのインタフェースを作った

--- azure/src/main/scala/com/example/azure/Handler.scala
+++ azure/src/main/scala/com/example/azure/Handler.scala
@@ -38,4 +38,27 @@ class Handler {
           .build
     }
   }
+
+  @FunctionName("HttpHandlerPOST")
+  def runHttpPost(@HttpTrigger(
+                    name = "req",
+                    methods = Array(HttpMethod.POST),
+                    authLevel = AuthorizationLevel.ANONYMOUS
+                  ) request: HttpRequestMessage[Requests],
+                  context: ExecutionContext): HttpResponseMessage = {
+    val logger = Logger(context.getLogger)
+    logger.info("Scala HTTP POST trigger processed a request.")
+
+    (for {
+      result <- Functions(logger).run(request.getBody)
+    } yield result) match {
+      case Right(s) =>
+        request.createResponseBuilder(HttpStatus.OK).body(s).build
+      case Left(e) =>
+        request
+          .createResponseBuilder(HttpStatus.BAD_REQUEST)
+          .body(e.toString)
+          .build
+    }
+  }
 }
