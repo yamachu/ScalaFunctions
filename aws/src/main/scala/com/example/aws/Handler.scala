0fec78530c09f6358377d395f702f36d998359ae
yamachu <yamachu.dev@gmail.com>
2019-01-27 17:08:17 +0900

    ResponseにStatusCodeとかを渡せるようにする
    ref: https://docs.aws.amazon.com/ja_jp/apigateway/latest/developerguide/handle-errors-in-lambda-integration.html

--- aws/src/main/scala/com/example/aws/Handler.scala
+++ aws/src/main/scala/com/example/aws/Handler.scala
@@ -2,18 +2,19 @@ package com.example.aws
 
 import com.amazonaws.services.lambda.runtime.{Context, RequestHandler}
 import com.example._
+import com.example.aws.objects.AWSResponse
 import com.example.aws.utils.Logger
-import com.example.objects.{Requests, Response}
+import com.example.objects.Requests
 
-class Handler extends RequestHandler[Requests, Response] {
-  def handleRequest(request: Requests, context: Context): Response = {
+class Handler extends RequestHandler[Requests, AWSResponse] {
+  def handleRequest(request: Requests, context: Context): AWSResponse = {
     val logger = Logger.instance
     logger.info("Scala HTTP POST trigger processed a request.")
 
     (for {
       result <- Functions(logger).run(request)
     } yield result) match {
-      case Right(s) => s
+      case Right(s) => AWSResponse(s)
       case Left(e)  => throw e
     }
   }
