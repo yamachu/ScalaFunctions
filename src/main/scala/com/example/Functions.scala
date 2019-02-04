04139e872987deeaaca6ad54322f40282632f109
yamachu <yamachu.dev@gmail.com>
2019-02-04 21:14:59 +0900

    うーん、なんでやろなー

--- src/main/scala/com/example/Functions.scala
+++ src/main/scala/com/example/Functions.scala
@@ -6,7 +6,7 @@ case class Functions(logger: utils.Logger) {
   def run(req: Requests): Either[Throwable, Response] = {
     logger.info("This is common Functions run")
     req.name match {
-      case "yamachu" => Right(Response("Hi, admin!"))
+      case "yamachu" => Right(Response("Hi, admin! Thank you for using"))
       case _         => Left(new Exception(s"require admin user name, but ${req.name}"))
     }
   }
