33143c59cc1e3b769369804fdee425e622bbcf72
yamachu <yamachu.dev@gmail.com>
2019-02-16 21:28:08 +0900

    APIのHandlerをAPIGatewayから渡ってくる感じのやつに変更

--- aws/src/main/scala/com/example/aws/Handler.scala
+++ aws/src/main/scala/com/example/aws/Handler.scala
@@ -1,21 +1,48 @@
 package com.example.aws
 
 import com.amazonaws.services.lambda.runtime.{Context, RequestHandler}
-import com.example._
-import com.example.aws.objects.AWSResponse
-import com.example.aws.utils.Logger
+import com.amazonaws.services.lambda.runtime.events.{
+  APIGatewayProxyRequestEvent,
+  APIGatewayProxyResponseEvent
+}
 import com.example.objects.Requests
+import com.example.Functions
+import com.example.aws.utils.Logger
+import io.circe.generic.auto._
+import io.circe.parser._
+import io.circe.syntax._
 
-class Handler extends RequestHandler[Requests, AWSResponse] {
-  def handleRequest(request: Requests, context: Context): AWSResponse = {
+import scala.collection.JavaConverters._
+
+class Handler extends RequestHandler[APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent] {
+  def handleRequest(input: APIGatewayProxyRequestEvent,
+                    context: Context): APIGatewayProxyResponseEvent = {
     val logger = Logger.instance
+
     logger.info("Scala HTTP POST trigger processed a request.")
 
     (for {
-      result <- Functions(logger).run(request)
+      json   <- decode[Requests](input.getBody)
+      result <- Functions(logger).run(json)
     } yield result) match {
-      case Right(s) => AWSResponse(s)
-      case Left(e)  => throw e
+      case Right(s) =>
+        new APIGatewayProxyResponseEvent()
+          .withStatusCode(200)
+          .withHeaders(
+            Map(
+              "Content-Type" -> "application/json",
+            ).asJava
+          )
+          .withBody(s.asJson.toString())
+      case Left(e) =>
+        new APIGatewayProxyResponseEvent()
+          .withStatusCode(500)
+          .withHeaders(
+            Map(
+              "Content-Type" -> "plain/text",
+            ).asJava
+          )
+          .withBody(e.toString)
     }
   }
 }
